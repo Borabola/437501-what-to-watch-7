6a224725a864e10559793111983aa0cb9d8a095b
Olya <osokolova1@gmail.com>
2021-06-06 14:43:49 +0300

    Добавляет компонент отрисовки стартовой страницы и карточки фильма

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Start from '../start/start';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({title, genre, releaseDate}) {
+  return (
+    <Start title={title} genre={genre} releaseDate={releaseDate}/>
+  );
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={'The Grand Budapest Hotel'} genre={'Drama'} releaseDate={'2014'}/>
   </React.StrictMode>,
   document.getElementById('root'));
