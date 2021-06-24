2d8bfa233c48f34e20f3d2acac90699b538079b6
Olya <osokolova1@gmail.com>
2021-06-24 09:54:51 +0300

    Правит изменение стейта в форме

--- project/src/components/page-footer/page-footer.jsx
+++ project/src/components/page-footer/page-footer.jsx
@@ -6,7 +6,7 @@ function PageFooter() {
   return (
     <footer className="page-footer">
       <div className="logo">
-        <Link to={AppRoute.MAIN} className="logo__link logo__link--light">
+        <Link to={AppRoute.ROOT} className="logo__link logo__link--light">
           <span className="logo__letter logo__letter--1">W</span>
           <span className="logo__letter logo__letter--2">T</span>
           <span className="logo__letter logo__letter--3">W</span>
--- project/src/components/pages/sing-in/sing-in.jsx
+++ project/src/components/pages/sing-in/sing-in.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../../logo/logo';
 import PageFooter from '../../page-footer/page-footer';
 
 
@@ -8,11 +9,7 @@ function SingIn() {
     <div className="user-page">
       <header className="page-header user-page__head">
         <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          <Logo />
         </div>
 
         <h1 className="page-title user-page__title">Sign in</h1>
